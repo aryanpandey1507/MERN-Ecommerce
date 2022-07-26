3160164cd3e4dbab6af80bf258413aba2be562cb
unknown <aryanpandey1507@gmail.com>
2022-07-27 00:14:48 +0530

    Added login and signup form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,8 @@ import ProductDetails from './components/Product/ProductDetails';
 import React from 'react';
 import Home from './components/Home/Home';
 import Products from './components/Product/Products.js'
-import Search from './components/Product/Search.js'
+import Search from './components/Product/Search.js';
+import LoginSignup from './components/User/LoginSignup';
 
 
 
@@ -32,6 +33,7 @@ function App() {
       <Route path="/products" element={<Products />} />
       <Route path="/products/:keyword" element={<Products />} />
       <Route path="/search" element={<Search />} />
+      <Route path="/login" element={<LoginSignup />} />
       
       </Routes>
       <Footer />
